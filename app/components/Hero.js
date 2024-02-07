a501a50aa4e4ae5ed3bfa83757f9e1917ad6e65b
Razin Tailor <razintailor@Razins-Mac-Studio.local>
2024-02-07 17:14:00 +0530

    typewriter bug fixed and added separate descriptor for ml and cv

--- app/components/Hero.js
+++ app/components/Hero.js
@@ -1,18 +1,14 @@
 "use client"
-import React, { useState, useEffect, useMemo } from 'react';
+import React, { useState, useEffect } from 'react';
 import Particles, { initParticlesEngine } from "@tsparticles/react";
 import { loadSlim } from "@tsparticles/slim";
-import { config } from '../tsParticleConfig';
-
-const phrases = ["Razin Tailor", "An Artist", "A Machine Learning/Computer Vision Enthusiast"];
-
+import { config } from '../tsParticleConfig';;
+import TypeWriter from 'typewriter-effect'
+const phrases = ["Razin Tailor", "An Artist", "Passionate about Machine Learning", "Intrigued by Computer Vision"];
 const Hero = () => {
     const [init, setInit] = useState(false);
-    const [phrase, setPhrase] = useState(phrases[0]);
-    const [phraseIndex, setPhraseIndex] = useState(0);
-    const options = useMemo(() => (config),[]);
+    const options = React.useMemo(() => (config), []);
 
-    // this should be run only once per application lifetime
     useEffect(() => {
         initParticlesEngine(async (engine) => {
             // you can initiate the tsParticles instance (engine) here, adding custom shapes or presets
@@ -24,44 +20,44 @@ const Hero = () => {
             //await loadBasic(engine);
         }).then(() => {
             setInit(true);
-        });
+        })
     }, []);
-    useEffect(() => {
-        const phraseInterval = setInterval(() => {
-            setPhraseIndex(i => (i + 1) % phrases.length);
-            setPhrase(phrases[phraseIndex]);
-        }, 4000); // Change phrase every 4 seconds
-        return () => {
-            clearInterval(phraseInterval);
-        };
-    }, [phraseIndex]);
 
     const particlesLoaded = (container) => {
         console.log(container);
     };
+
     return (
         <div className="relative">
             <div className="bg-[url('/razin-background.jpeg')] bg-cover bg-center h-screen flex flex-col justify-center items-center">
-            {/* For the black overlay over the background image */}
-            <div className="absolute inset-0 bg-black bg-opacity-65"></div> 
-                {init ? (<>
+                <div className="absolute inset-0 bg-black bg-opacity-65"></div>
+                {init ? (
                     <Particles
                         id="tsparticles"
                         particlesLoaded={particlesLoaded}
                         options={options}
                         className='absolute top-0 left-0 w-full h-full'
                     />
-                </>) : (<></>)}
-                <div className="z-10 max-w-lg">
+                ) : null}
+                <div className="z-10 max-w-3xl">
                     <h1 className="text-white text-center text-4xl sm:text-3xl">Welcome</h1>
-                    <div className="text-white text-center text-4xl ">I'M  <span className=' text-bold fade-in-out'>{phrase}</span></div>
+                    <div className="text-white text-center text-4xl ">
+                        I'M <TypeWriter
+                            options={
+                                {
+                                    strings: phrases,
+                                    autoStart: true,
+                                    loop: true
+                                }
+                            }
+                        />
+
+                    </div>
                     <p className="text-white text-center text-sm pt-10">Data is like people—interrogate it hard enough and it will tell you whatever you want to hear.</p>
                 </div>
             </div>
-
         </div>
     );
 };
 
 export default Hero;
-
